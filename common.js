fc86b92a273b16ddbc731158f54dc8e4a7afa1b6
and.borisov <and.borisov@agatgroup.com>
2020-03-17 17:12:16 +0300

    work-version

--- common.js
+++ common.js
@@ -1,10 +1,5 @@
-
 document.addEventListener('DOMContentLoaded', function () {
 
-// if (!modeEdit()) {
-	new ClipboardJS('.copy-icon'); //Покдлючаем копирование по кнопке
-	new ClipboardJS('.results__value'); //Покдлючаем копирование по полю с паролем
-// }
 	let url = 'query.php',
 		itemChange = document.querySelectorAll('.items__elem'),
 		subitemChange = document.querySelectorAll('.subitem li'),
@@ -17,6 +12,11 @@ document.addEventListener('DOMContentLoaded', function () {
 
 	function modeEdit() {
 		return strGET === 'admin';
+	}
+
+	if (!modeEdit()) {
+		new ClipboardJS('.copy-icon'); //Покдлючаем копирование по кнопке
+		new ClipboardJS('.results__value'); //Покдлючаем копирование по полю с паролем
 	}
 
 	/********Делаем открытие подменю************/
@@ -48,101 +48,106 @@ document.addEventListener('DOMContentLoaded', function () {
 		}, 200);
 	});
 
+	/********* Удаление класса change с подменю при использовнии поиска*************/
+	function removeChangeSubItem() {
+		subitemChange.forEach(function (item) {
+			if (item.classList.contains('change')) {
+				item.classList.remove('change');
+			}
+		});
+	}
+
+	/*********Удаляем существующий список сайтов**********/
+	function removeListPrompt() {
+		let promptItem = document.querySelectorAll('.prompt__item');
+		promptItem.forEach(function (item) {
+			item.remove();
+		});
+	}
+
+	function outputPrompt(elem) {
+		/**********Реализация выдачи подсказок с сайтами,  при использовании поиска********/
+		fieldSearch.addEventListener('input', function () {
+			let searchValue = this.value,
+				numberPrompt = 0,
+				count = 0;
+
+			removeListPrompt();
+
+			//Создаем новый список сайтов
+			for (let key of elem) {
+				let elLi = document.createElement('li');
+
+				if (key.NAME.match(searchValue) && count < 5) { //count ограничивает количество подсказок
+					elLi.className = 'prompt__item';
+					elLi.setAttribute('data-id', key.ID);
+					elLi.innerHTML = key.NAME;
+					promptList.append(elLi);
+					count++;
+				}
+			}
+			let promptItem = document.querySelectorAll('.prompt__item');
+			//Скрываем блок подсказок если поле пустое или если нет совпадений (подходящих сайтов)
+			if (searchValue === '' || !promptItem.length) promptBlock.classList.remove('open');
+			else promptBlock.classList.add('open');
+
+
+			// Выбор из предложенных результатов  с помощью кнопок up и down
+			fieldSearch.addEventListener('keydown', function (e) {
+
+				let promptItemSelect = document.querySelector('.prompt__item.active');
+
+				if (e.key === "Enter") {
+					fieldSearch.value = promptItemSelect.innerText;
+					promptBlock.classList.remove('open');
+					removeChangeSubItem();
+				}
+				if (e.code === 'ArrowDown' && numberPrompt + 1 < promptItem.length) {
+					numberPrompt++;
+					promptItem.forEach(function (item, i) {
+						if (i === numberPrompt) {
+							if (promptItemSelect) {
+								promptItem[numberPrompt - 1].classList.remove('active');
+							}
+							item.classList.add('active');
+						}
+					});
+				} else if (e.code === 'ArrowUp' && numberPrompt - 1 >= 0) {
+					numberPrompt--;
+					promptItem.forEach(function (item, i) {
+						if (i === numberPrompt) {
+							promptItem[numberPrompt + 1].classList.remove('active');
+							item.classList.add('active');
+						}
+					});
+				}
+			});
+			// Удаляем класс выделенной подсказки при навдении мыши
+			promptBlock.addEventListener('mouseover', function () {
+				numberPrompt = -1;
+
+				let promptItemSelect = document.querySelector('.prompt__item.active');
+
+				if (promptItemSelect) promptItemSelect.classList.remove('active');
+			});
+			// Подставляем выбранный результат в поле ввода
+			promptItem.forEach(function (item,) {
+				promptItem[0].classList.add('active');
+				item.addEventListener('click', function () {
+					fieldSearch.value = this.innerText;
+					removeChangeSubItem();
+				});
+			});
+		});
+	}
 
 	fetch(url, {cache: "no-cache"})
 		.then(response => response.json())
 		.then(elem => {
-
-			/**********Реализация выдачи подсказок с сайтами,  при использовании поиска********/
-			fieldSearch.addEventListener('input', function () {
-				let promptItem = document.querySelectorAll('.prompt__item'),
-					searchValue = this.value,
-					arrResult = [],
-					numberPrompt = 0,
-					count = 0;
-
-				//Удаляем существующий список сайтов
-				promptItem.forEach(function (item) {
-					item.remove();
-				});
-				//Создаем новый список сайтов
-				for (let key of elem) {
-					let elLi = document.createElement('li');
-
-					if (key.NAME.match(searchValue) && count < 5) { //count ограничивает количество подсказок
-						arrResult.push(key.NAME);
-						elLi.className = 'prompt__item';
-						elLi.setAttribute('data-id', key.ID);
-						elLi.innerHTML = key.NAME;
-						promptList.append(elLi);
-						count++;
-					}
-				}
-				promptItem = document.querySelectorAll('.prompt__item');
-				//Скрываем блок подсказок если поле пустое или если нет совпадений (подходящих сайтов)
-				if (searchValue === '' || !promptItem.length)
-					promptBlock.classList.remove('open');
-				else promptBlock.classList.add('open');
-
-
-				// Выбор из предложенных результатов  с помощью кнопок up и down
-				fieldSearch.addEventListener('keydown', function (e) {
-
-					let promptItemSelect = document.querySelector('.prompt__item.active');
-
-					if (e.key === "Enter") {
-						fieldSearch.value = promptItemSelect.innerText;
-						promptBlock.classList.remove('open');
-						removeChangeSubItem();
-					}
-					if (e.code === 'ArrowDown' && numberPrompt + 1 < promptItem.length) {
-						numberPrompt++;
-						promptItem.forEach(function (item, i) {
-							if (i === numberPrompt) {
-								if (promptItemSelect) {
-									promptItem[numberPrompt - 1].classList.remove('active');
-								}
-								item.classList.add('active');
-							}
-						});
-					} else if (e.code === 'ArrowUp' && numberPrompt - 1 >= 0) {
-						numberPrompt--;
-						promptItem.forEach(function (item, i) {
-							if (i === numberPrompt) {
-								promptItem[numberPrompt + 1].classList.remove('active');
-								item.classList.add('active');
-							}
-						});
-					}
-				});
-				// Удаляем класс выделенной подсказки при навдении мыши
-				promptBlock.addEventListener('mouseover', function () {
-					numberPrompt = -1;
-
-					let promptItemSelect = document.querySelector('.prompt__item.active');
-
-					if (promptItemSelect) promptItemSelect.classList.remove('active');
-				});
-				// Подставляем выбранный результат в поле ввода
-				promptItem.forEach(function (item,) {
-					promptItem[0].classList.add('active');
-					item.addEventListener('click', function () {
-						fieldSearch.value = this.innerText;
-						removeChangeSubItem();
-					});
-				});
-			});
-
-			/********* Удаление класса change с подменю при использовнии поиска*************/
-			function removeChangeSubItem() {
-				subitemChange.forEach(function (item) {
-					if (item.classList.contains('change')) {
-						item.classList.remove('change');
-					}
-				});
-			}
+			outputPrompt(elem);
 		})
 		.catch(error => alert(error));
+
 
 	let resultBlock = document.querySelector('.results__block');
 	// fieldSite = document.getElementById('popup-site'),
@@ -204,14 +209,14 @@ document.addEventListener('DOMContentLoaded', function () {
 		h3.className = 'subtitle';
 		h3.innerHTML = type;
 		// document.getElementById('result-' + type).appendChild(h3);
-		document.querySelector('.block__value[data-name="' + type +'"]').appendChild(h3);
+		document.querySelector('.block__value[data-name="' + type + '"]').appendChild(h3);
 	}
 
 	function createField(elId, type, fieldName, fieldVal) {
 		if (!modeEdit()) {
 			let str = '<div class="results__field" data-id="' + elId + '">\n' +
 				'<span class="name">' + fieldName + ':' + '</span>\n' +
-				'    <input type="text" class="results__value" data-clipboard-action="copy" id="field-' + elId +'" data-clipboard-target="#field-' + elId + '"\n' +
+				'    <input type="text" class="results__value" data-clipboard-action="copy" id="field-' + elId + '" data-clipboard-target="#field-' + elId + '"\n' +
 				'    data-id="' + elId + '" value="' + fieldVal + '"/>\n' +
 				'    <span class="copy-icon" data-clipboard-action="copy" data-clipboard-target="#field-' + elId + '">\n' +
 				'        <i class="ico-copy">\n' +
@@ -221,15 +226,15 @@ document.addEventListener('DOMContentLoaded', function () {
 				'        </i>\n' +
 				'        </span>\n' +
 				'</div>',
-				block = document.querySelector('.block__value[data-name="' + type +'"]');
+				block = document.querySelector('.block__value[data-name="' + type + '"]');
 			block.insertAdjacentHTML("beforeend", str);
 		} else {
 			let str = '<div class="results__field" data-id="' + elId + '">\n' +
 				'<input type="text" class="name" data-name="FIELD" data-id="' + elId + '" value="' + fieldName + '"/>\n' +
-				'    <input type="text" class="results__value"\n' +
+				'    <input type="text" class="results__value" data-clipboard-action="copy" data-clipboard-target="#field-' + elId + '"\n' +
 				'    data-id="' + elId + '" data-name="VALUE" value="' + fieldVal + '"/>\n' +
 				'</div>',
-				block = document.querySelector('.block__value[data-name="' + type +'"]');
+				block = document.querySelector('.block__value[data-name="' + type + '"]');
 			block.insertAdjacentHTML("beforeend", str);
 		}
 	}
@@ -254,67 +259,82 @@ document.addEventListener('DOMContentLoaded', function () {
 		});
 	}
 
+	/************Добавление блока со значениями (редактирование)************/
+	function addBlockValue() {
+		let str = `<div class="add-block">Добавить блок</div>`;
+
+		if (document.querySelector('.results__block .block__value')) {
+			let rbBlockValue = document.querySelectorAll('.results__block .block__value');
+			rbBlockValue.forEach(function (item, index) {
+				if (rbBlockValue.length === (index + 1)) item.insertAdjacentHTML("beforeend", str);
+			});
+		} else {
+			document.querySelector('.results__block').insertAdjacentHTML("beforeend", str);
+		}
+	}
+
+	/************Добавление поля со значениями (редактирование)************/
+	function addFieldValue() {
+		document.querySelectorAll('.block__value').forEach(function (el) {
+			let plus = document.createElement('div');
+
+			plus.className = 'add-field';
+			plus.innerText = '+';
+			el.append(plus);
+		});
+	}
+
+	/************Создание иконки удаления(крестик рядом с полем) (редактирование)************/
+	function createIconDelete() {
+		document.querySelectorAll('.results__field').forEach(function (el) {
+			let deleteIcon = document.createElement('span');
+
+			deleteIcon.className = 'delete-icon';
+			el.append(deleteIcon);
+		});
+	}
+
+	/************Вывод результатов************/
+	function outputResults(elem) {
+		valueTitle.innerText = elem[0].NAME;
+		valueTitle.setAttribute('href', elem[0].LINK);
+		valueTitle.setAttribute('data-id', elem[0].ID);
+		for (let k of elem) {
+			let blockVal = document.querySelector('.block__value[data-name="' + k.TYPE_NAME + '"]');
+			if (!blockVal) {
+				createValueBlock(k.TYPE_NAME, k.id_type);
+				createField(k.id_data, k.TYPE_NAME, k.FIELD, k.VALUE);
+			} else {
+				createField(k.id_data, k.TYPE_NAME, k.FIELD, k.VALUE);
+			}
+		}
+	}
+
 	let valueTitle = document.querySelector('a.title');
 
 	function sendData(data) {
-		let xhr = new XMLHttpRequest();
 
 		removeValue();
-		xhr.open('POST', 'query.php', true);
-		xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-		xhr.onload = function (e) {
-			if (xhr.status !== 200) {
-				alert(xhr.status + ': ' + xhr.statusText);
-			} else {
-				let elem = JSON.parse(xhr.responseText);
 
+		fetch(url, {
+			cache: "no-cache",
+			method: 'POST',
+			headers: {'Content-type': 'application/x-www-form-urlencoded'},
+			body: data
+		})
+			.then(response => response.json())
+			.then(elem => {
 				if (elem.length) {
-					valueTitle.innerText = elem[0].NAME;
-					valueTitle.setAttribute('href', elem[0].LINK);
-					valueTitle.setAttribute('data-id', elem[0].ID);
-					for (let k of elem) {
-						let blockVal = document.querySelector('.block__value[data-name="' + k.TYPE_NAME +'"]');
-						if (!blockVal) {
-							createValueBlock(k.TYPE_NAME, k.id_type);
-							createField(k.id_data, k.TYPE_NAME, k.FIELD, k.VALUE);
-						} else {
-							createField(k.id_data, k.TYPE_NAME, k.FIELD, k.VALUE);
-						}
-					}
-				}
-				else valueTitle.innerText = 'Нет данных';
+					outputResults(elem);
+				} else valueTitle.innerText = 'Нет данных';
 
 				if (modeEdit()) {
-					document.querySelectorAll('.block__value').forEach(function (el) {
-						let plus = document.createElement('div');
-
-						plus.className = 'add-field';
-						plus.innerText = '+';
-						el.append(plus);
-					});
-
-					document.querySelectorAll('.results__field').forEach(function (el) {
-						let deleteIcon = document.createElement('span');
-
-						deleteIcon.className = 'delete-icon';
-						el.append(deleteIcon);
-					});
-
-					let str = `<div class="add-block">Добавить блок</div>`;
-
-					if (document.querySelector('.results__block .block__value')) {
-						let rbBlockValue = document.querySelectorAll('.results__block .block__value');
-						rbBlockValue.forEach(function (item, index) {
-							if(rbBlockValue.length === (index + 1)) item.insertAdjacentHTML("beforeend", str);
-						});
-					} else {
-						document.querySelector('.results__block').insertAdjacentHTML("beforeend", str);
-					}
-
+					addFieldValue();
+					createIconDelete();
+					addBlockValue();
 				}
-			}
-		};
-		xhr.send(data);
+			})
+			.catch(error => alert(error));
 	}
 
 
@@ -345,9 +365,9 @@ document.addEventListener('DOMContentLoaded', function () {
 	if (modeEdit()) {
 		resultBlock.onclick = function (e) {
 			let target = e.target,
-				 siteId = target.closest('.results__block')
-				.firstElementChild
-				.getAttribute('data-id'),
+				siteId = target.closest('.results__block')
+					.firstElementChild
+					.getAttribute('data-id'),
 				arData = {};
 
 			if (target.className === 'add-field') {
@@ -389,19 +409,20 @@ document.addEventListener('DOMContentLoaded', function () {
 	}
 
 	function saveInput(data, id = '') {
-		let xhr = new XMLHttpRequest();
+		fetch(url, {
+			cache: "no-cache",
+			method: 'POST',
+			headers: {'Content-type': 'application/x-www-form-urlencoded'},
+			body: data
+		}).then(successResponse => {
 
-		xhr.open('POST', 'query.php', true);
-		xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-		xhr.onload = function (e) {
-			if (xhr.status !== 200) {
-				alert(xhr.status + ': ' + xhr.statusText);
+			if (successResponse.status !== 200) {
+				alert(successResponse.status + ': ' + successResponse.statusText);
 			} else {
 				if (id !== '') sendData("ID=" + id);
 			}
-
-		};
-		xhr.send(data);
+		})
+			.catch(error => alert(error));
 	}
 
 });
